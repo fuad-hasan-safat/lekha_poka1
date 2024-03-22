30e7ad0dc6ae2f2faa29f49f86ce5ec4d5641393
fuadsaft <fuadsafat16@gmail.com>
2024-03-23 01:14:23 +0600

    navbar transition done

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,35 +1,48 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { animations, motion } from "framer-motion";
+import { Select, Option } from "@material-tailwind/react";
+
 import Logo from "../common/Logo";
 import SearchIconInput from "./searchInput";
-import { Select, Option } from "@material-tailwind/react";
+import Image from "next/image";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const searchInputVariants = {
+  hidden: { opacity: 0, scaleX: 0, scaleY: 0 }, // Adjust scaleX for leftward growth
+  visible: { opacity: 1, scaleX: 1, scaleY: 1, transition: { duration: 0.9 } },
+};
+
 export const navLinks = [
-  { href: '/', label: 'Home' },
-  { href: '/about', label: 'About' },
-  { href: '/products', label: 'Products' },
-  { href: '/services', label: 'Services' },
-  { href: '/contact', label: 'Contact' },
-]
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/products", label: "Products" },
+  { href: "/services", label: "Services" },
+  { href: "/contact", label: "Contact" },
+];
 
 const MyNavbar = () => {
   const [selectedNav, setSelectedNav] = useState("");
- const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
   const [isActive, setIsActive] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(false);
 
-  const handleSearchClick = () => {
-    setIsActive(!isActive); // Toggle 'isActive' state on click
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
   };
 
   return (
     <>
-
-      <div className="fixed z-[999] bg-white-800 flex items-center justify-between shadow-dark-strong h-[92]  w-full bg-white">
+      <div className="fixed z-[999] bg-white-800 flex flex-row items-center  shadow-dark-strong h-[92]  w-full bg-white">
         {/* Logo */}
 
         <Logo
@@ -39,131 +52,109 @@ const MyNavbar = () => {
           alt="LekhaPoka logo"
         />
 
-
-        <div className="font-KangshaMJ flex flex-row space-x-3 text-black text-[18px]  place-content-center mr-[100px]">
+        <div
+          className={` space-x-3 text-black text-[18px]  place-content-center `}
+        >
           {/* Buttons */}
-          <div>
-            <ul className="space-x-6 flex flex-wrap kangsa-font">
-              <li
-                onClick={() => setSelectedNav("procchod")}
-                className={`${selectedNav === "procchod"
+
+          <ul
+            className={`flex flex-row space-x-6  kangsa-font transition-all ease-in-out duration-1000 ${
+              isSearchActive ? "ml-[400px]" : "ml-[600px]"
+            }`}
+          >
+            <li
+              onClick={() => setSelectedNav("procchod")}
+              className={`${
+                selectedNav === "procchod"
                   ? "text-yellow-400 font-semibold underline"
                   : ""
-                  }`}
+              }`}
+            >
+              <Link href="/">প্রচ্ছদ</Link>
+            </li>
+            <li>
+              {/* <Link href="/">সব লেখা</Link> */}
+              <button
+                className="text-black  w-[75px] hover:text-gray-900 focus:outline-none focus:ring-yellow-300 focus:ring-1  mb-4"
+                onClick={handleClick}
               >
-                <Link href="/">প্রচ্ছদ</Link>
-              </li>
-              <li>
-                {/* <Link href="/">সব লেখা</Link> */}
-                <button className="text-black hover:text-gray-900 focus:outline-none focus:ring-yellow-300 focus:ring-1  mb-4" onClick={handleClick}>
-                  সব লেখা
-                </button>
-                {isOpen && (
-                  <ul className="absolute  rounded shadow-md overflow-hidden space-y-3">
-                    {/* Dropdown content goes here */}
-                    <li className="bg-yellow-300 h-[40px] w-[60px] p-2 text-black rounded-md"><Link href={'/sob-kobita'} >কবিতা</Link> </li>
-                    <li className="bg-yellow-300 h-[40px] w-[60px] p-2 text-black rounded-md"><Link href={'/sob-golpo'} >গল্প</Link></li>
-                    <li className="bg-yellow-300 h-[40px] w-[60px] p-2 text-black rounded-md">জীবনী</li>
-                  </ul>
-                )}
-              </li>
-              <li
-                onClick={() => setSelectedNav("bistarito")}
-                className={`${selectedNav === "bistarito"
+                সব লেখা
+              </button>
+              {isOpen && (
+                <ul className="absolute  rounded shadow-md overflow-hidden space-y-3">
+                  {/* Dropdown content goes here */}
+                  <li className="bg-yellow-300 h-[40px] w-[60px] p-2 text-black rounded-md">
+                    <Link href={"/sob-kobita"}>কবিতা</Link>{" "}
+                  </li>
+                  <li className="bg-yellow-300 h-[40px] w-[60px] p-2 text-black rounded-md">
+                    <Link href={"/sob-golpo"}>গল্প</Link>
+                  </li>
+                  <li className="bg-yellow-300 h-[40px] w-[60px] p-2 text-black rounded-md">
+                    জীবনী
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li
+              onClick={() => setSelectedNav("bistarito")}
+              className={`${
+                selectedNav === "bistarito"
                   ? "text-yellow-400 font-semibold underline"
                   : ""
-                  }`}
-              >
-                <Link href="#">বিস্তারিত</Link>
-              </li>
-              <li
-                onClick={() => setSelectedNav("zogazog")}
-                className={`${selectedNav === "zogazog"
+              }`}
+            >
+              <Link href="#">বিস্তারিত</Link>
+            </li>
+            <li
+              onClick={() => setSelectedNav("zogazog")}
+              className={`${
+                selectedNav === "zogazog"
                   ? "text-yellow-400 font-semibold underline"
                   : ""
-                  }`}
-              >
-                <Link href="/contacts">যোগাযোগ</Link>
-              </li>
-              <li
-                onClick={() => setSelectedNav("amader_somporke")}
-                className={`${selectedNav === "amader_somporke"
+              }`}
+            >
+              <Link href="/contacts">যোগাযোগ</Link>
+            </li>
+            <li
+              onClick={() => setSelectedNav("amader_somporke")}
+              className={` w-[210px]${
+                selectedNav === "amader_somporke"
                   ? "text-yellow-400 font-semibold underline"
                   : ""
-                  }`}
-              >
-                <Link href="#">আমাদের সম্পর্কে</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="">
-            {/* <ul>
-              <li> */}
+              }`}
+            >
+              <Link
+              className="w-[210px]"
+               href="#">আমাদের সম্পর্কে</Link>
+            </li>
+            <li className="relative flex flex-row place-content-center ">
+              <Image
+                src="/images/svgs/search.svg"
+                height={50}
+                width={50}
+                alt=""
+                className={` cursor-pointer`}
+                onClick={() => setIsSearchActive(true)}
+              />
 
-            <SearchIconInput />
-
-            {/* <a href="#">
-                  {
-                    isSearchSelected ? <>
-                      <div className="flex flex-row ">
-                        <div className="transition duration-100 ease-in-out ... relative text-gray-600 focus-within:text-gray-400">
-                          <div className="absolute inset-y-0 left-0 flex items-center pl-2">
-                            <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
-                              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                            </button>
-                          </div>
-                          <input type="search" name="q" className=" py-2 text-sm shadow-4 text-white bg-transparent rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="অনুসন্ধান..." autoComplete="off" />
-                          <div className="absolute inset-y-0 left-4 flex items-center  z-50">
-                            <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
-                            <img
-                          height={iconl}
-                          width={iconl}
-                          onClick={() => setIsSearchSelected(false)}
-                          className="relative -right-[150px]"
-                          src="/images/svgs/list.svg" />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </> :
-                      <>
-                        <div >
-                          <img
-                            onClick={() => setIsSearchSelected(true)}
-                            src="/images/svgs/search.svg" />
-                        </div>
-                      </>
-                  } */}
-
-            {/* {!isSearchSelected ?
-                    <img
-                      height={60}
-                      width={60}
-                      onClick={() => setIsSearchSelected(true)}
-                      src="/images/svgs/search.svg" /> : <>
-                      <div className="flex flex-row">
-                        <img
-                          className="relative z-50 pr-4"
-                          src="/images/svgs/search.svg" />
-                        <input
-                          className="absolute border-solid  border-transparent shadow-lg rounded-xl p-2 h-10 w-[300px] text-sm "
-                          placeholder="           অনুসন্ধান"
-                          type="text">
-                        </input>
-                        <img
-                          height={iconl}
-                          width={iconl}
-                          onClick={() => setIsSearchSelected(false)}
-                          className="relative -right-[150px]"
-                          src="/images/svgs/list.svg" />
-                      </div>
-                    </>} */}
-            {/* </a> */}
-            {/* </li> */}
-            {/* Add more buttons as needed */}
-            {/* </ul> */}
-          </div>
-
+              <input
+                className={`w-[200px] bg-transparent text-black py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300 ${
+                  isSearchActive ? "visible" : "hidden"
+                }`}
+                type="text"
+                placeholder=" অনুসন্ধান..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+              {isSearchActive && (
+                <FontAwesomeIcon
+                  icon={faList}
+                  className="absolute text-gray-500 text-lg px-2 cursor-pointer ml-auto mt-2 right-5" // Right-aligned
+                  onClick={() => setIsSearchActive(false)}
+                />
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
--- components/navbar/searchInput.tsx
+++ components/navbar/searchInput.tsx
@@ -1,73 +1,58 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faList, faSearch } from '@fortawesome/free-solid-svg-icons';
-import Image from 'next/image';
+import React, { SetStateAction, useState } from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList, faSearch } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
-interface SearchIconInputProps { }
+interface SearchIconInputProps {
+  isSearchActive: boolean;
+  setIsSearchActive: React.Dispatch<SetStateAction<boolean>>;
+}
 
 const searchInputVariants = {
-    hidden: { opacity: 0, scaleX: 0 , scaleY:0}, // Adjust scaleX for leftward growth
-    visible: { opacity: 1, scaleX: 1, scaleY:1, transition: { duration: 0.9 } },
+  hidden: { opacity: 0, scaleX: 0, scaleY: 0 }, // Adjust scaleX for leftward growth
+  visible: { opacity: 1, scaleX: 1, scaleY: 1, transition: { duration: 0.9 } },
 };
 
-const SearchIconInput: React.FC<SearchIconInputProps> = () => {
-    const [isSearchActive, setIsSearchActive] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+const SearchIconInput: React.FC<SearchIconInputProps> = ({
+  isSearchActive,
+  setIsSearchActive,
+}) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-  
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(event.target.value);
-    };
+  return (
+    <div className="relative flex flex-row place-content-center transition-all duration-800">
+      <Image
+        src="/images/svgs/search.svg"
+        height={50}
+        width={50}
+        alt=""
+        className={` cursor-pointer`}
+        onClick={() => setIsSearchActive(true)}
+      />
 
-
-
-    return (
-        <motion.div 
-        variants={searchInputVariants}
-        className="relative place-content-center">
-            <Image
-            src="/images/svgs/search.svg"
-            height={50}
-            width={50}
-            alt=""
-            className={` cursor-pointer ${isSearchActive ? 'hidden' : ''}`}
-            onClick={()=> setIsSearchActive(true)}
-            />
-            {/* <FontAwesomeIcon
-                icon={faSearch}
-                className={`text-slate-500 text-lg cursor-pointer ${isSearchActive ? 'hidden' : ''}`}
-                onClick={()=> setIsSearchActive(true)}
-            /> */}
-
-            {isSearchActive && (
-            <FontAwesomeIcon
-                icon={faSearch}
-                className={`relative left-9 text-slate-500 text-lg cursor-pointer`}
-            />
-            )}
-            <motion.input
-                className={` bg-transparent text-black px-7 py-1 rounded-md ml-2 focus:outline-none focus:ring-2 focus:ring-gray-300 ${isSearchActive ? 'visible' : 'hidden'
-                    }`}
-                type="text"
-                placeholder=" অনুসন্ধান..."
-                value={searchTerm}
-                onChange={handleSearchChange}
-                variants={searchInputVariants}
-                initial="hidden"
-                animate={isSearchActive ? "visible" : "hidden"}
-            />
-            {isSearchActive && (
-                <FontAwesomeIcon
-                    icon={faList}
-                    className="absolute text-gray-500 text-lg px-2 cursor-pointer ml-auto mt-2 right-5" // Right-aligned
-                    onClick={()=> setIsSearchActive(false)}
-                />
-            )}
-        
-        </motion.div>
-    );
+      <input
+        className={`w-[200px] bg-transparent text-black py-1 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300 ${
+          isSearchActive ? "visible" : "hidden"
+        }`}
+        type="text"
+        placeholder=" অনুসন্ধান..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      {isSearchActive && (
+        <FontAwesomeIcon
+          icon={faList}
+          className="absolute text-gray-500 text-lg px-2 cursor-pointer ml-auto mt-2 right-5" // Right-aligned
+          onClick={() => setIsSearchActive(false)}
+        />
+      )}
+    </div>
+  );
 };
 
 export default SearchIconInput;
