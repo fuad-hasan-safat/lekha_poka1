a05d1a4dd04b901a4777f8d8d85e2863da7ff71e
fuadsaft <fuadsafat16@gmail.com>
2024-04-03 06:30:09 +0600

    music bad and rating star added to single post

--- app/(lekhapoka)/[slug]/page.jsx
+++ app/(lekhapoka)/[slug]/page.jsx
@@ -1,53 +1,51 @@
-"use client"
+"use client";
 import Sidebar from "@/components/sidebar/Sidebar";
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
-import FullPost from '@/components/common/fullContent'
-import RatingComponent from '@/components/common/starRating'
+import FullPost from "@/components/common/fullContent";
+import RatingComponent from "@/components/common/starRating";
 import AudioPlayer from "@/components/musicbar/AudioPlayer";
-import { fetchData } from '@/app/api/api'
+import { fetchData } from "@/app/api/api";
 
 export default function PostDetails(context) {
   const { slug } = context.params;
-  console.log('----slug-------', slug)
+  console.log("----slug-------", slug);
 
   const [data, setData] = useState([]); // State to store fetched data
   const [error, setError] = useState(null); // State to store any errors
-  const [isAudioAvailable, setIsAudioAvailAble] = useState(false)
-  const [audioSrc, setAudioSrc] = useState('');
-  const [audioData, setAudioData] = useState(null)
+  const [isAudioAvailable, setIsAudioAvailAble] = useState(false);
+  const [audioSrc, setAudioSrc] = useState("");
+  const [audioData, setAudioData] = useState(null);
 
-  const [rating, setRating] = useState(0)
-
-
+  const [rating, setRating] = useState(0);
 
   useEffect(() => {
-    console.log('<<<<<<<<<<<<<<<<<<<<<<-------------------------in side use effect----------------------->>>>>>>>>>>>>>>>')
-
-
+    console.log(
+      "<<<<<<<<<<<<<<<<<<<<<<-------------------------in side use effect----------------------->>>>>>>>>>>>>>>>"
+    );
 
     async function fetchDataAsync() {
       try {
-        const result = await fetchData(`http://192.168.88.248:3002/getpost/${slug}`);
-        console.log('result->>>>>>>>>>>>>>>>', result.object)
+        const result = await fetchData(
+          `http://192.168.88.248:3002/getpost/${slug}`
+        );
+        console.log("result->>>>>>>>>>>>>>>>", result.object);
         setData(result.object);
-        if(result.object.audio.length>0){
+        if (result.object.audio.length > 0) {
           setIsAudioAvailAble(true);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
 
     fetchDataAsync();
-
-
   }, []);
 
-  console.log('----DDDDDDDDD AAAAAAAAA TTTTTTTT AAAAAAAAAAA-------', data)
+  console.log("----DDDDDDDDD AAAAAAAAA TTTTTTTT AAAAAAAAAAA-------", data);
 
   return (
     <>
@@ -62,28 +60,33 @@ export default function PostDetails(context) {
         </div>
         <div className="flex flex-row">
           <div className="flex flex-col w-[70%]">
-            <FullPost content={data.content} title={data.title} writer={data.writer} />
+            <FullPost
+              content={data.content}
+              title={data.title}
+              writer={data.writer}
+            />
             <RatingComponent setRating={setRating} rating={rating} />
-           
           </div>
-
-         
 
           <div className="w-sidebarwidth">
             <Sidebar />
           </div>
         </div>
       </div>
-      {isAudioAvailable && <AudioPlayer 
-        playlist={[{
-          audioSrc: `http://192.168.88.248:3002/${data.audio}`,
-          metadata: {
-            title: data.title,
-            writer: data.writer,
-            image: '/images/writerimage/nazrul.jpg',
-          }
-        }]} 
-      />}
+      {isAudioAvailable && (
+        <AudioPlayer
+          playlist={[
+            {
+              audioSrc: `http://192.168.88.248:3002/${data.audio}`,
+              metadata: {
+                title: data.title,
+                writer: data.writer,
+                image: "/images/writerimage/nazrul.jpg",
+              },
+            },
+          ]}
+        />
+      )}
     </>
   );
 }
--- app/(lekhapoka)/kobita/[slug]/page.jsx
+++ app/(lekhapoka)/kobita/[slug]/page.jsx
@@ -1,30 +1,30 @@
-"use client"
+"use client";
 import Sidebar from "@/components/sidebar/Sidebar";
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
-import FullPost from '@/components/common/fullContent'
-import { fetchData } from '@/app/api/api'
-
+import FullPost from "@/components/common/fullContent";
+import { fetchData } from "@/app/api/api";
+import AudioPlayer from "@/components/musicbar/AudioPlayer";
 
 export default function PostDetails(context) {
   const { slug } = context.params;
-  console.log('----slug-------', slug)
+  console.log("----slug-------", slug);
 
   const [data, setData] = useState([]); // State to store fetched data
   const [error, setError] = useState(null); // State to store any errors
- 
-
 
   useEffect(() => {
-    console.log('in side use effect------------------------->>>>>>>>>>')
+    console.log("in side use effect------------------------->>>>>>>>>>");
 
     async function fetchDataAsync() {
       try {
-        const result = await fetchData(`http://192.168.88.248:3002/getpost/${slug}`);
-        console.log('result         ->>>>>>>>>>>>>>>>', result.object)
+        const result = await fetchData(
+          `http://192.168.88.248:3002/getpost/${slug}`
+        );
+        console.log("result         ->>>>>>>>>>>>>>>>", result.object);
         setData(result.object);
       } catch (error) {
         // Handle error
@@ -32,37 +32,50 @@ export default function PostDetails(context) {
     }
 
     fetchDataAsync();
-    // fetch(`http://192.168.88.248:3002/getpost/${slug}`)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     setData(data.object[0]);
-    //     //setHTML({ __html: data.object.content })
-    //     console.log('----get single post-------', data.object)
-    //   })
-    //   .catch(error => console.error("Error fetching data:", error));
-
   }, []);
- 
- // console.log('----DDDDDDDDD AAAAAAAAA TTTTTTTT AAAAAAAAAAA-------', data)
+
+  // console.log('----DDDDDDDDD AAAAAAAAA TTTTTTTT AAAAAAAAAAA-------', data)
 
   return (
-    <div className="flex flex-col pt-[115px]">
-      <div>
-        <Image
-          src={'/images/pages-banner-svg/kobita.svg'}
-          height={380}
-          width={1920}
-          alt={"kobita banner"}
-        />
-      </div>
-      <div className="flex flex-row">
-        
-      <FullPost content={data.content} title={data.title} writer={data.writer} />
-       
-        <div className="w-sidebarwidth">
-          <Sidebar />
+    <>
+      <div className="flex flex-col pt-[115px]">
+        <div>
+          <Image
+            src={"/images/pages-banner-svg/kobita.svg"}
+            height={380}
+            width={1920}
+            alt={"kobita banner"}
+          />
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col w-[70%]">
+            <FullPost
+              content={data.content}
+              title={data.title}
+              writer={data.writer}
+            />
+            <RatingComponent setRating={setRating} rating={rating} />
+          </div>
+
+          <div className="w-sidebarwidth">
+            <Sidebar />
+          </div>
         </div>
       </div>
-    </div>
+      {isAudioAvailable && (
+        <AudioPlayer
+          playlist={[
+            {
+              audioSrc: `http://192.168.88.248:3002/${data.audio}`,
+              metadata: {
+                title: data.title,
+                writer: data.writer,
+                image: "/images/writerimage/nazrul.jpg",
+              },
+            },
+          ]}
+        />
+      )}
+    </>
   );
 }
