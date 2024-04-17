00d9dc4784084019c301513f599319ccd0564d6b
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-04-17 12:55:59 +0600

    text line short fix done

--- components/common/fullContent copy.jsx
+++ components/common/fullContent copy.jsx
@@ -25,11 +25,7 @@ export default function FullPost({ content, title, writer, catagory }) {
                 <div className=" text-black pr-[100px] pt-24 space-y-3">
                     <div className="font-semibold text-[35px] text-[#FCD200]">{title}</div>
                     <div className="text-[22px] text-[#595D5B] ">{writer}</div>
-                    <div
-                        className="text-[16px] text-gray-500 text-justify"
-                        dangerouslySetInnerHTML={{ __html: content }}
-                    />
-
+                    <div className="text-[16px] text-gray-500 text-justify" dangerouslySetInnerHTML={{ __html: content }}/>
                 </div>
 
             </>
